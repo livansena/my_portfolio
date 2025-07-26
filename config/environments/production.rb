6c5a25ed61d8b825d7cb0d6fd4a7a74d9ce2108f
livansena <livansena@gmail.com>
2025-07-26 08:37:13 -0400

    Fix: Comment out require_master_key in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,28 +1,21 @@
-require "active_support/core_ext/integer/time"
+# config/environments/production.rb
 
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.require_master_key = true
+  # config.require_master_key = true
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
   config.assets.compile = false
   config.active_storage.service = :local
   config.force_ssl = true
-  config.logger = ActiveSupport::Logger.new(STDOUT).tap { |logger| logger.formatter = ::Logger::Formatter.new }.then { |logger| ActiveSupport::TaggedLogging.new(logger) }
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
-  config.log_tags = [ :request_id ]
-  config.action_mailer.perform_caching = false
-  config.i18n.fallbacks = true
-  config.active_support.report_deprecations = false
+  config.log_formatter = ::Logger::Formatter.new
+  config.logger = ActiveSupport::Logger.new("log/production.log")
+  config.logger.formatter = config.log_formatter
   config.active_record.dump_schema_after_migration = false
-
+  config.log_level = :info
+  config.log_tags = [ :request_id ]
   config.action_controller.asset_host = ENV['ASSET_HOST']
-
-  # config.hosts = [
-  #   "example.com",
-  #   /.*\.example\.com/
-  # ]
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # config.file_watcher = ActiveSupport::FileWatcher.new
 end
