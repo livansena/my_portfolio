0d95b2c2a39d42898ef2e63ee8f8571d19a2ae41
livansena <livansena@gmail.com>
2025-07-26 08:19:02 -0400

    Fix: Configure Puma for Render deployment

--- config/puma.rb
+++ config/puma.rb
@@ -1,44 +1,32 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# config/puma.rb
 
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# Define o número de threads (conexões simultâneas que um worker pode lidar)
+# Valores comuns: 5 a 16. Mantenha 5-5 para um aplicativo pequeno.
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 
-rails_env = ENV.fetch("RAILS_ENV") { "development" }
+# Define o número de workers (processos que rodam sua aplicação)
+# Em planos menores do Render (Free/Starter), 1 worker é comum.
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 
-if rails_env == "production"
-  # If you are running more than 1 thread per process, the workers count
-  # should be equal to the number of processors (CPU cores) in production.
-  #
-  # It defaults to 1 because it's impossible to reliably detect how many
-  # CPU cores are available. Make sure to set the `WEB_CONCURRENCY` environment
-  # variable to match the number of processors.
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
-  if worker_count > 1
-    workers worker_count
-  else
-    preload_app!
-  end
+# Conecta ao socket UNIX para melhor performance (Render configura isso)
+# ou usa a porta HTTP
+if ENV["RAILS_ENV"] == "production"
+  bind "unix:///tmp/puma.sock" # Render usa um socket UNIX
+else
+  port ENV.fetch("PORT") { 3000 } # Para desenvolvimento local
 end
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Define o ambiente da aplicação (production, development, test)
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Specifies the `environment` that Puma will run in.
-environment rails_env
+# Caminho para os logs do Puma
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/puma.pid" }
+state_path ENV.fetch("STATE_PATH") { "tmp/pids/puma.state" }
+stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# Reinicia workers que usam muita memória (útil para apps grandes)
+# prune_bundler
 
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
+# Tempo de espera para o shutdown dos workers
+# worker_timeout 30
