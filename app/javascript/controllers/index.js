e1a485c904c43c4d8edcc618f8a12ddba24168fa
livansena <livansena@gmail.com>
2025-07-26 23:07:56 -0400

    Removido menu hamburguer e ajustado navegacao para desktop/mobile

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,13 +2,8 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-// Importar e registrar cada controlador explicitamente (sem usar globs ou require.context)
-import ShowMoreCardsController from "controllers/show_more_cards_controller" // Note: sem "./" aqui
+import ShowMoreCardsController from "controllers/show_more_cards_controller"
 application.register("show-more-cards", ShowMoreCardsController)
 
-import MobileMenuController from "controllers/mobile_menu_controller" // Note: sem "./" aqui
-application.register("mobile-menu", MobileMenuController)
-
-// Se você tem hello_controller.js:
-import HelloController from "controllers/hello_controller" // Note: sem "./" aqui
+import HelloController from "controllers/hello_controller"
 application.register("hello", HelloController)
